7af38a1cc5ac42c578349aa5a608901440ac4446
sanjeeb sinkhada <Sanjeeb020@gmail.com>
2019-11-22 17:27:43 +0000

    mainHeader.vue

---
+++ src/components/main_header.vue
@@ -0,0 +1,28 @@
+<template>
+
+<nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <router-link :to="{name:'register'}">
+              <span class="glyphicon glyphicon-user"></span> Sign Up
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{name:'login'}">
+              <span class="glyphicon glyphicon-log-in"></span> Login
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+</template>
+
+<script>
+
+export default{
+ name:'main_header'
+
+}
+</script>
